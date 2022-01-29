717c74df034722cc03bca29e5275fce5ffe8b59b
Charalampos Anagnostou <babisanagnostou94@gmail.com>
2022-01-29 16:02:24 +0100

    update on Projects

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -2,10 +2,13 @@ export const Projects = [
   {
     thumbnail: "./images/cryptoapp.png",
     title: "Crypto App",
-    tags: ["React", "Redux","Ant Design"],
-    paragraph:["Cryptocurrency app with in-depth data of all the cryptocurrencies, markets and news.", "Coded using React and Redux toolkit for state manegment and Ant Design"],
-    codelink:"https://github.com/CharAnagn/Crypto-app",
+    tags: ["React", "Redux", "Ant Design"],
+    paragraph: [
+      "Cryptocurrency app with in-depth data of all the cryptocurrencies, markets and news.",
+      "Coded using React and Redux toolkit for state managment and Ant Design",
+    ],
+    codelink: "https://github.com/CharAnagn/Crypto-app",
     projectlink: "https://crypto-app-ch.netlify.app/",
-    projectlinkText: "View Project"
-  }
-]
+    projectlinkText: "View Project",
+  },
+];
